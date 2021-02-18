5040c10230bf912940fd3c7bb09900365e2663e3
bensantos <santosb@carleton.edu>
2021-02-18 11:10:14 -0700

    add cv2 code to turn image into video

---
+++ makevideo.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import os
+from natsort import natsorted
+#path = r"D:\Kaspar\images\monet\rocky"
+path = r"/home/ben/gans_git/pytorch-CycleGAN-and-pix2pix/results/style_vangogh_pretrained/test_latest/images"
+directory  = os.listdir(path)
+directory = natsorted(directory)
+img_array = []
+for filename in directory:
+    img = cv2.imread(path + '/' + filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+
+
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
